de3a47110181809bd4e2df1b29cd795b7bae55e1
Andres <j.andregon15@gmail.com>
2024-03-21 22:34:26 +0100

    menus finished

--- leaflet-map-app/src/App.js
+++ leaflet-map-app/src/App.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import SimpleMap from './SimpleMap';
-import { useState } from 'react';
+import React from "react";
+import SimpleMap from "./SimpleMap";
+import { useState } from "react";
 import axios from "axios";
 import L from "leaflet";
 
 function App() {
-   // new line start
-   const [profileData, setProfileData] = useState(null)
+  // new line start
+  const [profileData, setProfileData] = useState(null);
 
-   function getHumidityDailyAvg() {
-     axios({
-       method: "GET",
-       url:"/forecast/humidityDailyAvg",
-     })
-     .then((response) => {
-       const res =response.data
-       setProfileData(({
-         humidity: res.humidity}))
-     }).catch((error) => {
-       if (error.response) {
-         console.log(error.response)
-         console.log(error.response.status)
-         console.log(error.response.headers)
-         }
-     })}
+  function getHumidityDailyAvg() {
+    axios({
+      method: "GET",
+      url: "/forecast/humidityDailyAvg",
+    })
+      .then((response) => {
+        const res = response.data;
+        setProfileData({
+          humidity: res.humidity,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
 
   return (
     <div>
-      <h1>My Leaflet.js and React Map</h1>
+      <h1>Harvest</h1>
       <SimpleMap />
       {/* <p>To get your profile details: </p><button onClick={getHumidityDailyAvg}>Click me</button>
         {profileData && <div>
---
+++ leaflet-map-app/src/OverlayMenu.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+const OverlayMenu = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedDisease, setSelectedDisease] = useState("");
+
+  const handleOptionChange = (event) => {
+    const selected = event.target.value;
+    setSelectedOption(selected);
+    // Reset the disease selection when the main option changes
+    setSelectedDisease("");
+  };
+
+  const handleDiseaseChange = (event) => {
+    setSelectedDisease(event.target.value);
+  };
+
+  // Determine if the color bar should be displayed
+  const showColorBar =
+    selectedOption && (selectedOption !== "Disease Risk" || selectedDisease);
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "100px",
+        right: "30px",
+        zIndex: 1000,
+        backgroundColor: "rgba(255, 255, 255, 0.8)", // 80% transparent background
+        padding: "10px",
+        border: "1px solid #999",
+        borderRadius: "5px",
+        width: "250px", // Set a specific width for the entire menu
+      }}
+    >
+      <p style={{ margin: "0 0 10px" }}>Map Tools</p>
+      <div style={dropdownContainerStyle}>
+        <select
+          value={selectedOption}
+          onChange={handleOptionChange}
+          style={{ width: "100%" }}
+        >
+          <option value="">Select an Option</option>
+          <option value="NDVI">NDVI</option>
+          <option value="Disease Risk">Disease Risk</option>
+          <option value="Yield">Yield</option>
+        </select>
+
+        {selectedOption === "Disease Risk" && (
+          <select
+            value={selectedDisease}
+            onChange={handleDiseaseChange}
+            style={{ marginTop: "10px", width: "100%" }}
+          >
+            <option value="">Select a Disease</option>
+            <option value="Septoria Leaf Blotch">Septoria Leaf Blotch</option>
+            <option value="Powdery Mildew">Powdery Mildew</option>
+            <option value="Blackleg">Blackleg</option>
+            <option value="White Spot P.C.Spore">
+              White Leaf Spot P.C.Spore
+            </option>
+            <option value="Brown Rust">Brown Rust</option>
+          </select>
+        )}
+      </div>
+      {showColorBar && (
+        <div
+          style={{
+            ...elementStyle,
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ marginRight: "5px" }}>0</span>{" "}
+          {/* Number at the start of the color bar */}
+          <div
+            style={{
+              height: "20px",
+              flex: "1",
+              background: "linear-gradient(to right, #0000ff, #ff0000)",
+            }}
+          />
+          <span style={{ marginLeft: "5px" }}>1</span>{" "}
+          {/* Number at the end of the color bar */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+// Style for the container of dropdowns
+const dropdownContainerStyle = {
+  backgroundColor: "white",
+  padding: "10px",
+  borderRadius: "10px",
+  boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
+};
+
+// Style for individual elements like the color bar and its container
+const elementStyle = {
+  marginTop: "10px",
+  borderRadius: "10px",
+  boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
+  padding: "0 10px", // Padding added for the color bar container
+};
+
+export default OverlayMenu;
